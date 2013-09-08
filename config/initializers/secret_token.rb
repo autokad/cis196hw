b8681f853b8ac26eb396a9f69c072553cc0370c7
autokad <karl_dailey@hotmail.com>
2013-09-08 15:05:15 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cis196hw::Application.config.secret_token = '2cd0fa450aa4a543179ad52e1d3ae6e45d1619d389d0e479e77b707810304708eefd05f2a5b5bd322f4be6fbedea991bc415d7acf00d641456e2eb3b317fd68b'
